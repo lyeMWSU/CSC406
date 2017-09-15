5a987d0708478db630492b0efb34929b6a6f272d
lyeMWSU <lye@missouriwestern.edu>
2017-09-14 21:44:01 -0500

    Updated Homework

--- LisaHW/JavaMultiThreadWithPriorities/src/Main.java
+++ LisaHW/JavaMultiThreadWithPriorities/src/Main.java
@@ -9,31 +9,31 @@ public class Main {
 
         //create tasks
 
-        Runnable Gui = new GuiOut("Gui Poll", 600, output);
+        Runnable Gui = new GuiOut("Gui Poll\n\n", 600, output);
         Runnable WP = new WPOut('A', 50*10*2, output);
-//        Runnable DS = new DSOut();
-//        Runnable Printer = new PrintOut();
+        Runnable DS = new DSOut(2500, output);
+        Runnable Printer = new PrintOut(3600, output);
 
         //create threads
 
         Thread thread1 = new Thread(Gui);
         Thread thread2 = new Thread(WP);
-//        Thread thread3 = new Thread(DS);
-//        Thread thread4 = new Thread(Printer);
+        Thread thread3 = new Thread(DS);
+        Thread thread4 = new Thread(Printer);
 
         //priorities
 
-        thread1.setPriority(Thread.MIN_PRIORITY);
-        thread2.setPriority(Thread.MAX_PRIORITY);
-//        thread3.setPriority(Thread.NORM_PRIORITY);
-//        thread4.setPriority(Thread.NORM_PRIORITY);
+        thread1.setPriority(Thread.MAX_PRIORITY);
+        thread2.setPriority(Thread.MIN_PRIORITY);
+        thread3.setPriority(Thread.NORM_PRIORITY);
+        thread4.setPriority(Thread.NORM_PRIORITY);
 
         //Start Threads
 
         thread1.start();
         thread2.start();
-//        thread3.start();
-//        thread4.start();
+        thread3.start();
+        thread4.start();
 
         output.flush();
     }
@@ -60,11 +60,8 @@ class GuiOut implements Runnable{
             System.out.print(strings);
             out.print(strings);
             out.flush();
+            Thread.yield();
 
-            if (i % 2 == 0) {
-                System.out.println();
-                Thread.yield();
-            }
         }
     }
 }
@@ -85,37 +82,61 @@ class WPOut implements Runnable{
     //run method
 
     public void run(){
-        for (int i = 1; i <= n; i+=10){
-            System.out.println("WPx");
-            out.print("WPx");
-            out.flush();
-            Thread.yield();
+        for (int i = 1; i <= n; i++){
+            if (i%10==0){
+                System.out.print("WPx\n");
+                out.println("WPx\n");
+                Thread.yield();
+            }
+
         }
     }
 }
 
-//class DSOut implements Runnable{
-//    char chars;
-//    int n;
-//    PrintWriter out;
-//
-//
-//    //constructor
-//    public DSOut(char c, int t, PrintWriter output){
-//        chars = c;
-//        n = t;
-//        out = output;
-//    }
-//
-//    //run method
-//
-//    public void run(){
-//        for (int i = 1; i <= n; i+=10){
-//            System.out.println("WPx");
-//            Thread.yield();
-//
-//        }
-//    }
-//
-//
-//}
+class DSOut implements Runnable{
+    int n;
+    PrintWriter out;
+
+
+    //constructor
+    public DSOut(int t, PrintWriter output){
+        n = t;
+        out = output;
+    }
+
+    //run method
+
+    public void run(){
+        for (int i = 0; i <= n; i += 20){
+            System.out.println("DS" + i);
+            out.print("DS" + i);
+            if (i%20==0){
+                Thread.yield();
+            }
+        }
+    }
+}
+
+class PrintOut implements Runnable{
+    int n;
+    PrintWriter out;
+
+
+    //constructor
+    public PrintOut(int t, PrintWriter output){
+        n = t;
+        out = output;
+    }
+
+    //run method
+
+    public void run(){
+        for (int i = 0; i <= n; i++){
+            System.out.println("Print Line " + );
+            out.print();
+            if (i%60==0){
+                Thread.yield();
+            }
+        }
+    }
+}
