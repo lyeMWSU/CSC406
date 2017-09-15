65f405f7d69c2458bdd587bac60386fefd12ec86
Yuri <lye@missouriwestern.edu>
2017-09-14 23:08:37 -0500

    Update Homework2

--- LisaHW/JavaMultiThreadWithPriorities/src/Main.java
+++ LisaHW/JavaMultiThreadWithPriorities/src/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
         //create tasks
 
-        Runnable Gui = new GuiOut("Gui Poll\n\n", 600, output);
+        Runnable Gui = new GuiOut("Gui Poll", 600, output);
         Runnable WP = new WPOut('A', 50*10*2, output);
         Runnable DS = new DSOut(2500, output);
         Runnable Printer = new PrintOut(3600, output);
@@ -57,9 +57,8 @@ class GuiOut implements Runnable{
 
     public void run(){
         for (int i=1; i <= seconds; i++){
-            System.out.print(strings);
-            out.print(strings);
-            out.flush();
+            System.out.print(strings + "\n\n");
+            out.print(strings + "\n\n");
             Thread.yield();
 
         }
@@ -82,10 +81,10 @@ class WPOut implements Runnable{
     //run method
 
     public void run(){
-        for (int i = 1; i <= n; i++){
+        for (int i = 0; i <= n; i+=10){
             if (i%10==0){
-                System.out.print("WPx\n");
-                out.println("WPx\n");
+                System.out.print("WP" + i + "WPx" + "\n\n");
+                out.print("WP" + i + "WPx" + "\n\n");
                 Thread.yield();
             }
 
@@ -132,9 +131,11 @@ class PrintOut implements Runnable{
 
     public void run(){
         for (int i = 0; i <= n; i++){
-            System.out.println("Print Line " + );
-            out.print();
+            System.out.println("Print Line" + i);
+            out.print("Print Line" + i);
             if (i%60==0){
+                System.out.println();
+                out.println();
                 Thread.yield();
             }
         }
